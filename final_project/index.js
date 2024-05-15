7b100de83fbc03e762a348c9a33ee30ce6d498d8
DayanFranck <dayanfranck70.com>
2024-05-14 20:27:45 -0400

    Task 1

--- final_project/index.js
+++ final_project/index.js
@@ -3,20 +3,32 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
 const app = express();
-
 app.use(express.json());
-
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+    // Retrieve the access token from the session
+    const token = req.session.token;
 
+    // Check if the token exists
+    if (!token) {
+        return res.status(401).send('Access Denied: No token provided');
+    }
+
+    // Verify the token
+    jwt.verify(token, 'fingerprint_customer', (err, decoded) => {
+        if (err) {
+            return res.status(401).send('Access Denied: Invalid token');
+        } else {
+            // If everything is good, save the request for use in other routes
+            req.user = decoded;
+            next();
+        }
+    });
+});
+
+const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
